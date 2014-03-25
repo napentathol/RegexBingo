84e06dbe4a43a18d8d33fb10c1e420423b1ba2db
Alex <ac.webster1990@gmail.com>
2014-03-24 23:55:11 -0500

    modifying assumption test

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
-app.get('/test/',test.test.handleRequest);
+app.get('/test',test.test.handleRequest);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
