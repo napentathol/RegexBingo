1547c49bc70f5665873de4e565ef1fb2542e7451
Alex <ac.webster1990@gmail.com>
2014-03-26 22:22:15 -0500

    maybe this'll fix it

--- model/UserModel.js
+++ model/UserModel.js
@@ -3,25 +3,28 @@
  */
 
 
-exports = {
-    list : [],
-    createUser : function() {
-        var user = {
-            key : exports.createKey(),
-            username : "Some User"
-        };
-        exports.list.push(user);
-        return user;
-    },
-    createKey : function() {
-        var key="";
-        for(var i=0;i<64;i++){
-            key+=exports.seed.charAt(exports.randomInt(0,63))
-        }
-        return key;
-    },
-    randomInt: function(min, max){
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    },
-    seed:"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890.-"
+exports.list = [];
+
+exports.createUser = function() {
+    var user = {
+        key : exports.createKey(),
+        username : "Some User"
+    };
+    exports.list.push(user);
+    return user;
 };
+
+exports.createKey = function() {
+    var key="";
+    for(var i=0;i<64;i++){
+        key+=exports.seed.charAt(exports.randomInt(0,63))
+    }
+    return key;
+};
+
+exports.randomInt = function(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+exports.seed = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890.-";
+
