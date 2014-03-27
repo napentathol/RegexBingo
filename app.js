f19fa3757e4cbd5fc22133a55fa2ce4502138366
Alex <ac.webster1990@gmail.com>
2014-03-26 23:03:37 -0500

    Adding the cookie parser I forgot.

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
+app.use(express.cookieParser());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
