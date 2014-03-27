090b1d896fbcd34de994072a11b51cacf0b68bb4
Alex <ac.webster1990@gmail.com>
2014-03-27 00:13:29 -0500

    Moving Header code up in the execution order. Derp.

--- routes/IndexRoute.js
+++ routes/IndexRoute.js
@@ -7,12 +7,12 @@ exports.createObject = function(userModel){
     return {
         index : function(req, res){
             var user = userModel.findUserFromRequest(req);
-            res.render('index', { username: user.username });
             res.cookie('regexKey', user.key, {
                 maxAge: 360000,
                 path: '/',
                 httpOnly: false
             });
+            res.render('index', { username: user.username });
         }
     };
 };
