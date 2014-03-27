d4725d0ede487b14595bcbe70ce2f7280daadc22
Alex <ac.webster1990@gmail.com>
2014-03-26 21:57:35 -0500

    index now creates users

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,11 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+exports.createObject = function(userModel){
+    return {
+        index : function(req, res){
+            userModel.createUser();
+            res.render('index', { title: 'Express' });
+        }
+    };
 };
